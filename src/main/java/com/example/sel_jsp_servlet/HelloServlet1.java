dc9cd1af856ca6240cea0c82cb2f5df24469783a
wcjeong <wcjeong@hist.co.kr>
2021-03-11 12:59:12 +0900

    2nd commit

---
+++ src/main/java/com/example/sel_jsp_servlet/HelloServlet1.java
@@ -0,0 +1,31 @@
+package com.example.sel_jsp_servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet1", value = "/HelloServlet1")
+public class HelloServlet1 extends HttpServlet {
+
+    private String message1;
+
+    public void init() { message1 = "hello servlet"; }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message1 + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
